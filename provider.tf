0ddcfdbd99e1583e509efedb66977d609e5dbe54
Surabhi Maheshwari <30389878+surabhimaheshwari@users.noreply.github.com>
2022-11-15 10:53:05 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region = "us-west-2" 
 environment {
       AWS_ACCESS_KEY_ID = "${secret.TF_VAR_aws_access_key}"
-      AWS_ACCESS_KEY_ID = "${secret.TF_VAR_aws_secret_key}"
+      AWS_SECRET_KEY_ID = "${secret.TF_VAR_aws_secret_key}"
     }
 }
