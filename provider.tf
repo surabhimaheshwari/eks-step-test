e8e408b4803c0fa0a66e56b30ca4d02115108169
Surabhi Maheshwari <30389878+surabhimaheshwari@users.noreply.github.com>
2022-11-15 11:17:20 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,8 @@
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
+
 provider "aws" {
   region = "us-west-2" 
-  access_key = "${var.TF_VAR_aws_access_key}"
-  secret_key = "${var.TF_VAR_aws_secret_key}"
+  access_key = "${var.AWS_ACCESS_KEY_ID}"
+  secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
 }
