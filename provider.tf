4005091149a48da0860f1474266aa047aa42a0b7
Surabhi Maheshwari <30389878+surabhimaheshwari@users.noreply.github.com>
2022-11-15 11:01:48 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "us-west-2" 
-  access_key = "${secret.TF_VAR_aws_access_key}"
-  secret_key = "${secret.TF_VAR_aws_secret_key}"
+  access_key = "${var.TF_VAR_aws_access_key}"
+  secret_key = "${var.TF_VAR_aws_secret_key}"
 }
